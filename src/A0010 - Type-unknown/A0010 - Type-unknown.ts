ed6d4e720c9fd5045ed6c21a690d104a5f5a43bb
GabrielPrompt <gabrielue37@gmail.com>
2024-09-06 15:20:46 -0300

    last commit

---
+++ src/A0010 - Type-unknown/A0010 - Type-unknown.ts
@@ -0,0 +1,8 @@
+let x: unknown;
+x = 10;
+x = 'Luis'
+x = 90
+
+const y = 80;
+
+if (typeof x == 'number')  console.log(x + y)
---
+++ src/A009-Type-Enum/A009-Type-Enum.ts
@@ -0,0 +1,13 @@
+enum Cores {
+    VERMELHO,
+    AMARELO,
+    ROSA,
+}
+
+enum Cores {
+    ROXO= 100,
+    AZUL = 40,
+    VERDE = 'NADA',
+}
+
+console.log(Cores)
